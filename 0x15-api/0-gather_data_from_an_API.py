77063b484cece4fc35323097f78571e9097ac1dd
Darren <darren2097@gmail.com>
2023-01-16 17:43:15 +0200

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""script that, using this REST API, for a given employee ID, returns
-information about his/her TODO list progress"""
+# script to gather data from an API
 import requests
 import sys
 
