1c726d427248bd353344e3e28e377f6e4ba87b60
Darren <darren2097@gmail.com>
2023-01-16 17:40:34 +0200

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""script that, using this REST API, for a given employee ID, returns
+information about his/her TODO list progress"""
+import requests
+import sys
+
+
+def get_uname(url, user_id):
+    """Gets username"""
+
+    response = requests.get(
+            "{}users/{}".format(url, user_id))
+    usr_dict = response.json()
+    return usr_dict['name']
+
+
+def get_todo_list(url, user_id):
+    """Gets todo list"""
+
+    response = requests.get(
+            "{}users/{}/todos".format(url, user_id))
+    return response.json()
+
+
+def get_completed(todo_list):
+    """Gets completed items"""
+
+    done_str = ""
+    done_list = []
+    count = 0
+
+    for todo in todo_list:
+        if todo['completed'] is True:
+            count += 1
+            done_list.append("\t {}".format(todo['title']))
+    done_str = "\n".join(done_list)
+
+    return count, done_str
+
+
+if __name__ == '__main__':
+    user_id = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/'
+
+    uname = get_uname(url, user_id)
+    todo_list = get_todo_list(url, user_id)
+    total = len(todo_list)
+
+    count, done_str = get_completed(todo_list)
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        uname, count, total))
+    if count > 0:
+        print(done_str)
